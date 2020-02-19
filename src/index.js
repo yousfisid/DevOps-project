acbca945bd5bafffb18848c0237ae31f75e476a8
sergkudinov <sergkudinov@gmail.com>
2020-02-19 16:57:48 +0100

    index: port

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const userRouter = require('./routes/users')
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = 3003
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
