31d5cc9b2f699900b430d025062063f968ba145a
sergkudinov <sergkudinov@gmail.com>
2020-02-19 16:39:21 +0100

    cinfig update

--- src/configure.js
+++ src/configure.js
@@ -1,10 +1,16 @@
 const path = require('path')
 const mixme = require('mixme')
+const fs = require('fs')
 config_default = require('../conf/default')
 
 module.exports = (config_user = {}) => {
   config = mixme.merge(config_default, config_user)
   if (config.users.db_dir)
     config.users.db_dir = path.join(__dirname, '..', config.users.db_dir)
+
+
+  // Create directory if not exists
+  if (!fs.existsSync(config.users.db_dir))
+    fs.mkdirSync(config.users.db_dir)
   return config
 }
